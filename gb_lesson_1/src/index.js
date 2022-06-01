4332ea541c80376887fe51d444838a011b0349f3
[Kiplord] <uakuznetsov2015@gmail.com>
2022-06-01 19:45:19 +0300

    Delete commented text

--- gb_lesson_1/src/index.js
+++ gb_lesson_1/src/index.js
@@ -2,18 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-// const p = React.createElement(
-//   'p',
-//   {},
-//   'Кажется, мы подключили React'
-// )
-
-// const h1 = React.createElement(
-//   'h1',
-//   {className: 'element'},
-//   p
-// );
-//////////////////////////////////////////////////////////////////////////////
 
 
 
