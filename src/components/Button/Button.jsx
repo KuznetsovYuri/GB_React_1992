5fa0671a042a2fcde578d656592b4d7c4f643655
[Kiplord] <uakuznetsov2015@gmail.com>
2022-06-17 23:02:17 +0300

    install light house, some small chages

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,15 @@
 import MUIButton from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-export const Button = ({ label, disabled = false, click = () => null }) => {
+
+export const Button = ({ disabled = false, click = () => null }) => {
     return (
         <MUIButton
             disabled={disabled}
             onClick={click}
             variant="contained"
             type="submit"
-            
             endIcon={<SendIcon />}>
-            
-            {label}
+
         </MUIButton>
     );
 };
